14eda10f3911807eec09a2075e789eb86e36757f
Beau Beauchamp, WebTigers <beau@webtigers.com>
2021-02-09 17:18:35 -0500

    Load Migrations from Package

--- src/ProductsServiceProvider.php
+++ src/ProductsServiceProvider.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tipoff\Products;
 
 use Spatie\LaravelPackageTools\Package;
@@ -8,6 +10,12 @@ use Tipoff\Products\Commands\ProductsCommand;
 
 class ProductsServiceProvider extends PackageServiceProvider
 {
+    public function boot()
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        parent::boot();
+    }
+
     public function configurePackage(Package $package): void
     {
         /*
@@ -17,9 +25,6 @@ class ProductsServiceProvider extends PackageServiceProvider
          */
         $package
             ->name('products')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_products_table')
-            ->hasCommand(ProductsCommand::class);
+            ->hasConfigFile();
     }
 }
