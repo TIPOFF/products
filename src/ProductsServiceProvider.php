43ce9a5632f8b5a83bab7f028a279ad7c6e74aae
Drew Roberts <github@drewroberts.com>
2021-02-14 07:29:28 -0500

    Remove models & policies since not created yet

--- src/ProductsServiceProvider.php
+++ src/ProductsServiceProvider.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Tipoff\Products;
 
-use Tipoff\Products\Models\Product;
-use Tipoff\Products\Policies\ProductPolicy;
 use Tipoff\Support\TipoffPackage;
 use Tipoff\Support\TipoffServiceProvider;
 
@@ -14,9 +12,6 @@ class ProductsServiceProvider extends TipoffServiceProvider
     public function configureTipoffPackage(TipoffPackage $package): void
     {
         $package
-            ->hasPolicies([
-                Product::class => ProductPolicy::class,
-            ])
             ->name('products')
             ->hasConfigFile();
     }
