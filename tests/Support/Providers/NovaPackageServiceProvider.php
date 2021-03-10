946932032d9ac09601b0a1571f32548c035033c2
Wolfred Montilla <wolfrednicolas@gmail.com>
2021-03-10 10:01:17 -0500

    add nova product to the nova package service provider

--- tests/Support/Providers/NovaPackageServiceProvider.php
+++ tests/Support/Providers/NovaPackageServiceProvider.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace Tipoff\Products\Tests\Support\Providers;
 
 use Tipoff\TestSupport\Providers\BaseNovaPackageServiceProvider;
+use Tipoff\Products\Nova\Product;
 
 class NovaPackageServiceProvider extends BaseNovaPackageServiceProvider
 {
+	Product::class,
 }
