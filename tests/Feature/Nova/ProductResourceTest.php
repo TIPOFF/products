4a3a77e1c7a240fd9be98df580b3d87feaa500d1
Wolfred Montilla <wolfrednicolas@gmail.com>
2021-03-24 11:56:21 -0400

    add index test for nova product

--- tests/Feature/Nova/ProductResourceTest.php
+++ tests/Feature/Nova/ProductResourceTest.php
@@ -7,34 +7,29 @@ namespace Tipoff\Products\Tests\Feature\Nova;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tipoff\Products\Models\Product;
 use Tipoff\Products\Tests\TestCase;
+use Spatie\Permission\Models\Role;
+use Tipoff\Authorization\Models\User;
 
 class ProductResourceTest extends TestCase
 {
     use DatabaseTransactions;
-
+    
+    private const NOVA_ROUTE = 'nova-api/products';
+    
     /** @test */
     public function index()
     {
-        Product::factory()->count(4)->create();
+        Product::factory()->count(3)->create();
 
-        $this->actingAs(self::createPermissionedUser('view products', true));
+        /** @var User $user */
+        $user = User::factory()->create()->givePermissionTo(
+            Role::findByName('Admin')->getPermissionNames()     // Use individual permissions so we can revoke one
+        );
+        $this->actingAs($user);
 
-        $response = $this->getJson('nova-api/products')
+        $response = $this->getJson(self::NOVA_ROUTE)
             ->assertOk();
 
-        $this->assertCount(4, $response->json('resources'));
-    }
-
-    /** @test */
-    public function show()
-    {
-        $product = Product::factory()->create();
-
-        $this->actingAs(self::createPermissionedUser('view products', true));
-
-        $response = $this->getJson("nova-api/products/{$product->id}")
-            ->assertOk();
-
-        $this->assertEquals($product->id, $response->json('resource.id.value'));
+        $this->assertCount(3, $response->json('resources'));
     }
 }
